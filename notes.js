0a24ac8257bd11935d692e8a5ff5149f730d0262
Ian <ianfan0@gmail.com>
2015-02-14 15:36:20 +0000

    added a proper header; got sessionStorage working better; worked out why js wasn't loading; improved navigation; added some media queries

--- notes.js
+++ notes.js
@@ -1,22 +1,12 @@
 'use strict';
-window.notes = {
-}
+window.notes = {};
 
-window.onpopstate = function() {
-    console.log(window.location)
-    var state = JSON.parse(sessionStorage.getItem(window.location));
-    document.title = state.title;
+notes.ondomcontentload = function() {
+    notes.ondomcontentload = null; // delete function (not needed any more)
 
-    document.getElementsByTagName('main')[0].innerHTML = state.main;
-
-    var scrollPos = document.getElementById('header').offsetHeight + 2;
-    if (window.scrollY > scrollPos) window.scroll(0, scrollPos);
-};
-
-document.addEventListener('DOMContentLoaded', function() {
     var stylesheet = document.createElement('link');
     stylesheet.rel = 'stylesheet';
-    stylesheet.href = 'notesextra.css';
+    stylesheet.href = '/gcse/notesjs.css';
     document.head.appendChild(stylesheet);
 
     var sidebar = document.getElementsByTagName('nav')[0];
@@ -42,19 +32,20 @@ document.addEventListener('DOMContentLoaded', function() {
             }
         }
     }, true);
+
     if (history.pushState) {
-        notes.cachePage = function(dom) {
-            console.log(window.location)
-            sessionStorage.setItem(window.location, JSON.stringify({
-                title: dom.title,
-                main: dom.getElementsByTagName('main')[0].innerHTML
+        var cachePage = function(dom) {
+            sessionStorage.setItem(window.location.pathname, JSON.stringify({
+                main: dom.getElementsByTagName('main')[0].innerHTML,
+                prev: dom.getElementsByClassName('prev')[0].href,
+                next: dom.getElementsByClassName('next')[0].href
             }));
         };
 
         history.replaceState('', '');
-        notes.cachePage(document);
+        cachePage(document);
 
-        sidebar.addEventListener('click', function(event) {
+        var openPage = function(event) {
             event.preventDefault();
             event.stopPropagation();
 
@@ -63,23 +54,9 @@ document.addEventListener('DOMContentLoaded', function() {
             if (!url || link.className === 'current') return;
 
             history.pushState('', '', url);
-            
-            function loadFromCache() {
+
+            if (sessionStorage.getItem(link.pathname)) {
                 window.onpopstate();
-
-                var current = document.getElementsByClassName('current');
-                for (var i = current.length; i;) {
-                    current[--i].className = 'expanded';
-                }
-
-                link.className = 'current';
-                while ((link = link.parentNode).tagName !== 'NAV') {
-                    if (link.tagName === 'OL') link.className = 'current';
-                }  
-            }
-
-            if (sessionStorage.getItem(url)) {
-                loadFromCache();
                 return;
             }
 
@@ -89,14 +66,63 @@ document.addEventListener('DOMContentLoaded', function() {
                 if (request.status >= 200 && request.status < 400) {
                     var newDoc = document.implementation.createHTMLDocument();
                     newDoc.documentElement.innerHTML = request.responseText;
-                    notes.cachePage(newDoc);
-                    loadFromCache();
-                } else {
-
-                }
+                    cachePage(newDoc);
+                    window.onpopstate();
+                } else {}
             };
             request.onerror = function() {};
             request.send();
-        }, true);
+        };
+
+        sidebar.addEventListener('click', openPage, true);
+        document.getElementsByTagName('header')[0].addEventListener('click',
+                                                                     openPage);
     }
-});
+
+    var button = document.getElementsByTagName('button')[0];
+    button.className = 'arrow';
+    button.onclick = function(event) {
+        this.className = this.className === 'arrow' ? 'hamburger' : 'arrow';
+    };
+};
+
+document.addEventListener('DOMContentLoaded', notes.ondomcontentload);
+
+window.onpopstate = function() {
+    var current = document.getElementsByClassName('current');
+    for (var i = current.length; i;) {
+        current[--i].className = 'expanded';
+    }
+
+    var path = window.location.pathname,
+        state = JSON.parse(sessionStorage.getItem(path)),
+        main = document.getElementsByTagName('main')[0];
+    main.innerHTML = state.main;
+    
+    document.title = main.getElementsByTagName('h1')[0].textContent;
+
+    function replace(which) {
+        var els = document.getElementsByClassName('title-' + which);
+        els[0].textContent = els[1].textContent;
+        if (state[which]) els[0].previousElementSibling.href = state[which];
+        else els[0].previousElementSibling.removeAttribute('href');
+    }
+    replace('prev');
+    replace('next');
+
+    var link = document.querySelector('[href="' + path + '"]');
+    link.className = 'current';
+    for (; link.tagName !== 'NAV'; link = link.parentNode) {
+        if (link.tagName === 'OL') link.className = 'current';
+    }
+
+    // + 2 to account for box shadow radius
+    var currPos = document.getElementsByTagName('header')[0].offsetHeight + 2;
+    if (window.scrollY > currPos) window.scroll(0, currPos);
+};
+
+window.setTimeout(function() {
+    if (document.readyState in {complete: 1, interactive: 1, loaded: 1} &&
+            notes.ondomcontentload) notes.ondomcontentload();
+}, 0); // put this at the end of execution queue in case DOMContentLoaded
+       // handler is queued
